de19376deb1aa91dc957a94b201fed071c20e1cb
rehrazo <rehrazo@gmail.com>
2026-02-22 14:09:29 -0600

    Create ProductList.vue component

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
     <h1>Product List</h1>
-    <!-- Product items will go here -->
+    <div v-for="product in products" :key="product.id">
+      <product-card :product="product" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ProductList',
-}
+  data() {
+    return {
+      products: [],
+    };
+  },
+  components: {
+    'product-card': () => import('./ProductCard.vue'),
+  },
+  mounted() {
+    this.fetchProducts();
+  },
+  methods: {
+    fetchProducts() {
+      // Fetch products from API
+    },
+  },
+};
 </script>
+
+<style scoped>
+/* Styles for ProductList.vue */
+</style>
